df147ce54049a5f70aaeebcc12212ddccdb730e2
MaxMilu <sydyzyc2@163.com>
2022-06-20 23:07:35 +0800

    添加model注释

--- model/gta/vehicle.go
+++ model/gta/vehicle.go
@@ -2,34 +2,34 @@ package gta
 
 type Vehicle struct {
 	ID              int     //
-	Category        string  //
-	CategoryName    string  //
-	Brand           string  //
-	Name            string  //
+	Category        string  // 类别英文名
+	CategoryName    string  // 类别中文名
+	Brand           string  // 品牌
+	Name            string  // 名称
 	URL             string  //
-	Type            string  //
-	Conditional     string  //
-	Speed           float64 //
-	Acceleration    float64 //
-	Braking         float64 //
-	Handling        float64 //
+	Type            string  // 载具类别
+	Conditional     string  // 获取条件
+	Speed           float64 // 速度
+	Acceleration    float64 // 加速度
+	Braking         float64 // 刹车
+	Handling        float64 // 操控
 	TopSpeed        bool    //
 	TopAcceleration bool    //
 	TopBraking      bool    //
 	TopHandling     bool    //
-	ForSale         bool    //
-	Website         string  //
-	Cost            string  //
-	Seats           int     //
+	ForSale         bool    // 可购买
+	Website         string  // 购买网站
+	Cost            string  // 价格
+	Seats           int     // 座位数量
 	Personal        bool    //
 	Premium         bool    //
-	Moddable        bool    //
-	SuperModdable   bool    //
-	Sellable        bool    //
-	SellPrice       string  //
-	MainImageURL    string  //
+	Moddable        bool    // 可改装
+	SuperModdable   bool    // 可超级改装
+	Sellable        bool    // 可出售
+	SellPrice       string  // 出售价格
+	MainImageURL    string  // 主图片
 	MainImage       []byte  //
-	ActionImageURL  string  //
+	ActionImageURL  string  // 副图片
 	ActionImage     []byte  //
 }
 
